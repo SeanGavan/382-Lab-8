d097637d547eee6338acba49863e7e2f554a284b
SeanGavan <C16Sean.Gavan@usafa.edu>
2014-12-12 00:19:56 -0700

    Code update

--- Code/main.c
+++ Code/main.c
@@ -1,21 +1,23 @@
 #include <msp430.h>
 
 void moveForward(void);		// declaring methods for later
-void moveBackward(void);
-void smallLeft(void);
-void smallRight(void);
-void bigLeft(void);
-void bigRight(void);
-void enable(void);
+void leftTurn(void);
+void rightTurn(void);
+void getCenter(void);
+void getLeft(void);
+void isCenterClose(void);
+void isLeftClose(void);
+void stop(void);
+
+unsigned short centerSample[16];
+unsigned short leftSample[16];
+unsigned int t = 0;
 
 int THRESHOLDLEFT = 0x3B0;		// Threshold voltage for wall prescence
 int THRESHOLDCENTER = 0x3D0;
 
 void main(void) {
     WDTCTL = WDTPW | WDTHOLD;				  // Stop watchdog timer
-    ADC10CTL0 = ADC10SHT_3 + ADC10ON + ADC10IE; 		  // 64 ADC clks to combat loading, ADC10ON
-    ADC10AE0 |= BIT1 + BIT2 + BIT4;           // P1.1, P1.2, P1.4 to ADC mode (A1,A2,A4)
-    ADC10CTL1 |= ADC10SSEL1|ADC10SSEL0;       // Select SMCLK
 
     P2DIR |= BIT2;							// P2.2 is associated with TA1CCR1
     P2SEL |= BIT2;							// P2.2 is associated with TA1CCTL1
@@ -45,14 +47,78 @@ void main(void) {
     TA1CCTL2 = OUTMOD_3;					// set TACCTL1 to Reset / Set mode
 
     while (1) {
-
+    	getCenter();
+    	isCenterClose();
     } // end loop
 } // end main
 
-void enable(void) {			// sets the enables for the motors
-    P2OUT |= BIT0;
-    P2OUT |= BIT3;
+void getCenter(void){
+	t = 0;
+	ADC10CTL0 = 0;											// Turn off ADC
+	ADC10CTL1 = INCH_2;						// Use channel A2
+	ADC10AE0 = BIT2;										// Enable P1.2 as ADC input
+	ADC10CTL0 = SREF_0 | ADC10SHT_3 | ADC10ON | ENC;		// Vcc & Vss as reference
+	ADC10CTL0 |= ADC10SC;									// ADC conversion
+	ADC10CTL0 &= ~ENC;
+	while(ADC10CTL1 & ADC10BUSY);
+	centerSample[t] = ADC10MEM;
 }
+
+void isCenterClose(void){
+	while(centerSample[t]<THRESHOLDCENTER){
+		getLeft();
+		isLeftClose();
+		centerSample[t] = 0xFFFF;
+	}
+//	if(centerSample[t]<THRESHOLDCENTER){
+//		stop();
+//		getLeft();
+//		isLeftClose();
+//	}
+	moveForward();
+}
+
+void getLeft(void){
+	t = 0;
+	ADC10CTL0 = 0;											// Turn off ADC
+	ADC10CTL1 = INCH_1;						// Use channel A1
+	ADC10AE0 = BIT1;										// Enable P1.1 as ADC input
+	ADC10CTL0 = SREF_0 | ADC10SHT_3 | ADC10ON | ENC;		// Vcc & Vss as reference
+	ADC10CTL0 |= ADC10SC;									// ADC conversion
+	ADC10CTL0 &= ~ENC;
+	while(ADC10CTL1 & ADC10BUSY);
+	leftSample[t] = ADC10MEM;
+}
+
+void isLeftClose(void){
+	while(leftSample[t]>THRESHOLDLEFT){
+		leftTurn();
+		leftSample[t] = 0;
+		return;
+	}
+	while(leftSample[t]<THRESHOLDLEFT){
+		rightTurn();
+		leftSample[t] = 0xFFFF;
+		return;
+	}
+//	if(leftSample[t]>THRESHOLDLEFT){
+//		leftTurn();
+//	} else {
+//		rightTurn();
+//	}
+}
+
+void stop(void){
+	t = 0;
+	while(t<0xFFFF){
+		P2OUT &= ~BIT0;
+		P2OUT &= ~BIT3;
+		t++;
+	}
+	P2OUT |= BIT0;
+	P2OUT |= BIT3;
+}
+
 void moveForward(void) {
 
 	int t = 0;				// sets time for moving forward
@@ -61,83 +127,28 @@ void moveForward(void) {
 	    P2OUT &= ~BIT5;		// R motor CW
 		t++;
 	}
-
-    P2OUT &= ~BIT0;			// stop motors to show next step is working
-    P2OUT &= ~BIT3;
-
 }
 
-void moveBackward(void) {
-
-    TA1CCR1 = 0x0100;		// set motors to be the same speed
-    TA1CCR2 = 0x0100;
+void leftTurn(void) {
 
 	int t = 0;
+	TA1CCR1 = 0x0050;
 	while (t<0xFFFF){
-	    P2OUT &= ~BIT1;		// L motor CW
-	    P2OUT |= BIT5;		// R motor CCW
+		P2OUT &= ~BIT1;
+		P2OUT &= ~BIT5;
 		t++;
 	}
+	TA1CCR1 = 0x0020;
+}
 
-    P2OUT &= ~BIT0;
-    P2OUT &= ~BIT3;
+void rightTurn(void) {
 
-    TA1CCR1 = 0x0020;		// set the motors back to normal speed
+	int t = 0;
+    TA1CCR2 = 0x0030;
+	while (t<0xFFFF){
+		P2OUT |= BIT1;
+		P2OUT |= BIT5;
+		t++;
+	}
     TA1CCR2 = 0x0020;
-
-}
-
-void smallLeft(void) {
-
-	int t = 0;
-	while (t<0xAFFF){		// less time for <45 degree angle
-		P2OUT &= ~BIT1;
-		P2OUT &= ~BIT5;
-		t++;
-	}
-
-    P2OUT &= ~BIT0;
-    P2OUT &= ~BIT3;
-
-}
-
-void smallRight(void) {
-
-
-	int t = 0;
-	while (t<0xAFFF){
-		P2OUT |= BIT1;
-		P2OUT |= BIT5;
-		t++;
-	}
-
-    P2OUT &= ~BIT0;
-    P2OUT &= ~BIT3;
-
-}
-
-void bigLeft(void) {
-
-	int t = 0;
-	while (t<0xFFFF){		// more time for a >45 degree angle
-		P2OUT &= ~BIT1;
-		P2OUT &= ~BIT5;
-		t++;
-	}
-
-    P2OUT &= ~BIT0;
-    P2OUT &= ~BIT3;
-}
-
-void bigRight(void) {
-
-	int t = 0;
-	while (t<0xFFFF){
-		P2OUT |= BIT1;
-		P2OUT |= BIT5;
-		t++;
-	}
-
-    P2OUT &= ~BIT0;
-    P2OUT &= ~BIT3;
 }
